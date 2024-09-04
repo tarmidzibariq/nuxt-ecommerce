c38ac2b3817602050a09d9340a68457f8950d024
tarmidzibariq <tarmidzibariq30@gmail.com>
2024-09-05 06:22:10 +0700

    Installasi dan Konfigurasi Nuxt Auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,8 +60,76 @@ export default {
     'bootstrap-vue/nuxt',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    //https://dev.auth.nuxtjs.org/
+    '@nuxtjs/auth-next',
   ],
 
+  auth: {
+    strategies: {
+
+        //strategy "admin"
+        admin: {
+          scheme: 'local',
+          token: {
+            property: 'token',
+            required: true,
+            type: 'Bearer'
+          },
+          user: {
+            property: 'user',
+            // autoFetch: true
+          },
+          endpoints: {
+            login: {
+              url: '/api/admin/login',
+              method: 'post',
+              propertyName: 'token'
+            },
+            logout: {
+              url: '/api/admin/logout',
+              method: 'post'
+            },
+            user: {
+              url: '/api/admin/user',
+              method: 'get',
+              propertyName: 'user'
+            }
+          },
+        },
+
+        //strategy "customer"
+        customer: {
+          scheme: 'local',
+          token: {
+            property: 'token',
+            required: true,
+            type: 'Bearer'
+          },
+          user: {
+            property: 'user',
+            // autoFetch: true
+          },
+          endpoints: {
+            login: {
+              url: '/api/customer/login',
+              method: 'post',
+              propertyName: 'token'
+            },
+            logout: {
+              url: '/api/customer/logout',
+              method: 'post'
+            },
+            user: {
+              url: '/api/customer/user',
+              method: 'get',
+              propertyName: 'user'
+            }
+          },
+        },
+
+    },
+  },
+  
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
