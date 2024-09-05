ff6a359b97a63a9f58fc0ead95e6fc5e6fb7929b
tarmidzibariq <tarmidzibariq30@gmail.com>
2024-09-05 07:00:27 +0700

    Membuat Middleware Role

---
+++ middleware/isCustomer.js
@@ -0,0 +1,15 @@
+export default function({ $auth, redirect }) {
+
+    //check loggedIn "false"
+    if(!$auth.loggedIn) {
+        return redirect('/customer/login')
+    }
+
+    //check customer role
+    if($auth.strategy.name != "customer") {
+        return redirect('/customer/login')
+    } else {
+        return
+    }
+
+}
