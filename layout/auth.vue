0f1c93eca50c565c764d07174a0018f00162de5c
tarmidzibariq <tarmidzibariq30@gmail.com>
2024-09-09 11:32:28 +0700

    Membuat Proses Login Admin

---
+++ layout/auth.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="c-app flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-4">
+
+            <Nuxt />
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+
+  }
+</script>
+
+<style>
+  body {
+    font-family: 'Quicksand', sans-serif;
+    font-size: initial!important;
+  }
+
+  .form-control {
+    height: initial;
+    font-size: 1rem;
+  }
+
+  .c-app {
+      background-color: #BCCAD8!important;
+  }
+</style>
