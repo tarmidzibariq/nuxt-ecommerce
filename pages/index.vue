46c179af8db562f211db6de64eabdba39ba56bd7
tarmidzibariq <tarmidzibariq30@gmail.com>
2024-10-07 07:48:21 +0700

    Membuat Layout Default

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <div class="mt-custom mb-5">
+    <div class="fade-in">
+      <h1 class="text-center">SANTRI KODING</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+export default {}
 </script>
