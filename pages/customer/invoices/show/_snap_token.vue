7ed8999d02d1b2baf210ca2b87b680fc037720b5
tarmidzibariq <tarmidzibariq30@gmail.com>
2024-10-09 15:16:34 +0700

    Membuat Fitur Rating dan Review

--- pages/customer/invoices/show/_snap_token.vue
+++ pages/customer/invoices/show/_snap_token.vue
@@ -95,12 +95,12 @@
                     <td>
                       <button @click="payment(invoice.snap_token)" v-if="invoice.status == 'pending'"
                         class="btn btn-info">BAYAR SEKARANG</button>
-                      <button v-else-if="invoice.status == 'success'"
-                        class="btn btn-success"><i class="fa fa-check-circle"></i> {{ invoice.status }}</button>
-                      <button v-else-if="invoice.status == 'expired'"
-                        class="btn btn-warning-2"><i class="fa fa-exclamation-triangle"></i> {{ invoice.status }}</button>
-                      <button v-else-if="invoice.status == 'failed'"
-                        class="btn btn-danger"><i class="fa fa-times-circle"></i> {{ invoice.status }}</button>
+                      <button v-else-if="invoice.status == 'success'" class="btn btn-success"><i
+                          class="fa fa-check-circle"></i> {{ invoice.status }}</button>
+                      <button v-else-if="invoice.status == 'expired'" class="btn btn-warning-2"><i
+                          class="fa fa-exclamation-triangle"></i> {{ invoice.status }}</button>
+                      <button v-else-if="invoice.status == 'failed'" class="btn btn-danger"><i
+                          class="fa fa-times-circle"></i> {{ invoice.status }}</button>
                     </td>
                   </tr>
                 </client-only>
@@ -132,6 +132,48 @@
                             <td style="padding: .20rem"><b>{{ order.qty }}</b></td>
                           </tr>
                         </table>
+                        <!-- modal button -->
+                        <button v-if="invoice.status == 'success'" type="button" class="btn btn-warning-2 mt-4"
+                          data-toggle="modal" :data-target="'#modal-'+order.id">
+                          BERIKAN ULASAN
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" ref="modal" :id="'modal-'+order.id" tabindex="-1" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">ULASAN PRODUK</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                <div class="row justify-content-center">
+                                  <div class="col-md-7">
+                                    <vue-star-rating v-model="rating.star" :show-rating="false">
+                                    </vue-star-rating>
+                                  </div>
+                                </div>
+                                <div class="row mt-4">
+                                  <div class="col-md-12">
+                                    <div class="form-group">
+                                      <label class="font-weight-bold">ULASAN</label>
+                                      <textarea class="form-control" id="alamat" rows="3"
+                                        placeholder="Masukkan Ulasan Produk" v-model="rating.review"></textarea>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">TUTUP</button>
+                                <button v-if="rating.star && rating.review"
+                                  @click.prevent="storeReview(order.id, order.product.id)" type="button"
+                                  class="btn btn-warning" data-dismiss="modal">KIRIM</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </td>
                       <td class="b-none text-right">
                         <p class="m-0 font-weight-bold">Rp. {{ formatPrice(order.price) }}</p>
@@ -150,9 +192,9 @@
 </template>
 <script>
   // import sidebar
-import Sidebar from '@/components/web/sidebar.vue';
+  import Sidebar from '@/components/web/sidebar.vue';
 
-export default {
+  export default {
 
     // middleware
     middleware: 'isCustomer',
@@ -162,62 +204,115 @@ export default {
 
     // register components
     components: {
-        Sidebar
+      Sidebar
     },
 
     // meta
     head() {
-        return {
-            title: 'Detail Order - Customer',
-        }
+      return {
+        title: 'Detail Order - Customer',
+      }
     },
 
     // hook "asyncData"
-    async asyncData({ store, route }) {
-        await store.dispatch('customoer/invoice/getDetailInvoice', route.params.snap_token)
+    async asyncData({
+      store,
+      route
+    }) {
+      await store.dispatch('customoer/invoice/getDetailInvoice', route.params.snap_token)
     },
 
     // computed
     computed: {
-        invoice() {
-            return this.$store.state.customer.invoice.invoice
-        }
+      invoice() {
+        return this.$store.state.customer.invoice.invoice
+      }
     },
 
     methods: {
 
-        // methods "payment"
-        payment(snap_token) {
-            window.snap.pay(snap_token, {
-                onSuccess : function () {
-                    router.push({
-                        name: 'invoices-show-snap_token',
-                        params: {
-                            snap_token: snap_token
-                        }
-                    })
-                },
-                
-                onPending : function () {
-                    router.push({
-                        name: 'invoices-show-snap_token',
-                        params: {
-                            snap_token: snap_token
-                        }
-                    })
-                },
-                
-                onError : function () {
-                    router.push({
-                        name: 'invoices-show-snap_token',
-                        params: {
-                            snap_token: snap_token
-                        }
-                    })
-                },
-                
+      // methods "payment"
+      payment(snap_token) {
+        window.snap.pay(snap_token, {
+          onSuccess: function () {
+            router.push({
+              name: 'invoices-show-snap_token',
+              params: {
+                snap_token: snap_token
+              }
             })
-        }
+          },
+
+          onPending: function () {
+            router.push({
+              name: 'invoices-show-snap_token',
+              params: {
+                snap_token: snap_token
+              }
+            })
+          },
+
+          onError: function () {
+            router.push({
+              name: 'invoices-show-snap_token',
+              params: {
+                snap_token: snap_token
+              }
+            })
+          },
+
+        })
+      },
+
+      // method "storeReview"
+      async asyncData(orderId, productId) {
+
+        // define formData
+        let formData = new FormData();
+
+        formData.append('rating', this.rating.star)
+        formData.append('review', this.rating.review)
+        formData.append('order_id', this.rating.order_id)
+        formData.append('product_id', this.rating.product_id)
+
+        // sending data to action "storeReview" vuex
+        await this.$store.dispatch('customer/review/storeReview', formData)
+
+          // success
+          .then(() => {
+
+            // refresh data
+            this.$nuxt.refresh()
+
+            // clear state
+            this.rating.star = 0
+            this.rating.review = ''
+
+            // sweet alert
+            this.$swal.fire({
+              title: 'BERHASIL!',
+              text: "Ulasan Berhasil Disimpan!",
+              icon: 'success',
+              showConfirmButton: false,
+              timer: 3000
+            })
+
+              // redirect route same page
+              this.$router.push({ path: this.$route.path });
+          })
+          .catch(() => { 
+
+              // sweet alert
+            this.$swal.fire({
+              title: 'GAGAL!',
+              text: "Anda sudah membuat ulasan untuk produk ini!",
+              icon: 'error',
+              showConfirmButton: false,
+              timer: 3000
+            })
+          })
+      }
     }
   }
+
 </script>
