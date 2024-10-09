7d680b8931d1f04ae41a0fc0b0ba2d713bbb2f01
tarmidzibariq <tarmidzibariq30@gmail.com>
2024-10-10 04:00:58 +0700

    Membuat Vuex Web Slider

---
+++ store/web/slider.js
@@ -0,0 +1,44 @@
+// state
+export const state = () => ({
+
+  // sliders
+  sliders: []
+})
+
+// mutations
+export const mutations = {
+
+  // mutation "SET_SLIDERS_DATA"
+  SET_SLIDERS_DATA(state, payload) {
+
+    // set value state "sliders"
+    state.sliders = payload
+  }
+}
+
+// actions 
+export const actions = {
+
+  // get sliders data
+  getSlidersData({
+    commit
+  }) {
+
+    // set promise
+    return new Promise((resolve, reject) => {
+
+      // fetching Rest API "/api/web/sliders" with method "GET"
+      this.$axios.get('/api/web/sliders')
+
+        // success
+        .then((response) => {
+
+          // commit mutation "SET_SLIDERS_DATA"
+          commit('SET_SLIDERS_DATA', response.data.data)
+
+          // resolve promise
+          resolve()
+        })
+    })
+  }
+}
