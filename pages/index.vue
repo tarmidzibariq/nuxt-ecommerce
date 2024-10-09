15575bf68f6098b8356bf0c27ec319b328283436
tarmidzibariq <tarmidzibariq30@gmail.com>
2024-10-10 04:14:13 +0700

    Membuat dan Menampilkan Component Slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,54 @@
 <template>
   <div class="mt-custom mb-5">
     <div class="fade-in">
-      <h1 class="text-center">SANTRI KODING</h1>
+
+      <!-- Slider -->
+      <Slider />
+      <!-- End Slider -->
+
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+  // import slider
+  import Slider from '@/components/web/slider.vue'
+
+  export default {
+
+    // register component
+    components: {
+      Slider
+    },
+
+    // meta
+    head() {
+      return {
+        title: 'MI STORE - Distributor Xiaomi Indonesia Resmi',
+        meta: [{
+            hid: 'og:title',
+            name: 'og:title',
+            content: 'MI STORE - Distributor Xiaomi Indonesia Resmi'
+          },
+          {
+            hid: 'og:site_name',
+            name: 'og:site_name',
+            content: 'MI STORE - Distributor Xiaomi Indonesia Resmi'
+          },
+          {
+            hid: 'og:image',
+            name: 'og:image',
+            content: '/image/logo.png'
+          },
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Official Toko Online Penjualan Produk Xiaomi'
+          },
+        ]
+      }
+    }
+
+  }
+
 </script>
